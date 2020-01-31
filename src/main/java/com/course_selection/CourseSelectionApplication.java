bbd9448253d44f7df17adf1796a1e92cf2d54512
Alicecy <1790144051@qq.com>
2020-01-31 16:50:14 +0800

    新增留言板和投诉功能

--- src/main/java/com/course_selection/CourseSelectionApplication.java
+++ src/main/java/com/course_selection/CourseSelectionApplication.java
@@ -7,5 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class CourseSelectionApplication {
     public static void main(String[] args) {
         SpringApplication.run(CourseSelectionApplication.class, args);
+
+
     }
 }
